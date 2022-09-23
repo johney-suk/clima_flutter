d720f47078d016c60bdd091c2a6ed82f9948c1fd
hsuk <sukhyunil@naver.com>
2022-09-23 09:57:29 +0900

    added image

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clima_flutter/screens/location_screen.dart';
 import 'package:clima_flutter/ui/loding_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,3 +8,16 @@ void main() {
     home: const LoadingScreen(),
   ));
 }
+
+class ClimaApp extends StatelessWidget {
+  const ClimaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const LocationScreen(),
+    );
+  }
+}
+
