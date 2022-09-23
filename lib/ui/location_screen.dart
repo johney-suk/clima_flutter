39e3e8f2d9610d8b44b3d285f3b820bc8fdc0662
hsuk <sukhyunil@naver.com>
2022-09-23 15:26:33 +0900

    added

--- lib/ui/location_screen.dart
+++ lib/ui/location_screen.dart
@@ -1,9 +1,51 @@
-import 'package:clima_flutter/utilities/constants.dart';
+import 'package:clima_flutter/services/weather.dart';
+import 'package:clima_flutter/ui/city_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:clima_flutter/utilities/constants.dart';
 
+class LocationScreen extends StatefulWidget {
 
-class LocationScreen extends StatelessWidget {
-  const LocationScreen({Key? key}) : super(key: key);
+  late var weatherData;
+
+  LocationScreen({Key? key, this.weatherData}) : super(key: key);
+
+  @override
+  State<LocationScreen> createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+
+  late int temperature;
+  late String cityName;
+  late String weatherIcon;
+  late String weatherMessage;
+
+  void _updateUI() {
+    // 1. 이전화면에서 전달된 데이터를 확보
+    var weather = widget.weatherData;
+    //print('Location Screen: \n$weather');
+    // 2. 전달된 데이터는 JSON 데이터 이다.
+    // 따라서, JSON 을 파싱하여 원하는 데이터를 뽑아낸다.
+    // 온도, 날씨아이콘, 도시명
+    print(weather['name']);
+    print(weather['main']['temp'].toInt());
+    print(weather['weather'][0]['id']);
+
+    setState(() {
+      temperature = weather['main']['temp'].toInt();
+      cityName = weather['name'];
+      weatherIcon =
+          WeatherModel().getWeatherIcon(weather['weather'][0]['id']);
+      weatherMessage = WeatherModel().getMessage(weather['main']['temp'].toInt());
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _updateUI();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +76,11 @@ class LocationScreen extends StatelessWidget {
                     ),
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        return CityScreen();
+                      }));
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
@@ -47,11 +93,11 @@ class LocationScreen extends StatelessWidget {
                 child: Row(
                   children: <Widget>[
                     Text(
-                      '32°',
+                      '$temperature°',
                       style: kTempTextStyle,
                     ),
                     Text(
-                      '☀️',
+                      weatherIcon,
                       style: kConditionTextStyle,
                     ),
                   ],
@@ -60,7 +106,7 @@ class LocationScreen extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.only(right: 15.0),
                 child: Text(
-                  "It's 🍦 time in San Francisco!",
+                  "$weatherMessage in $cityName!",
                   textAlign: TextAlign.right,
                   style: kMessageTextStyle,
                 ),
--- lib/ui/loding_screen.dart
+++ lib/ui/loding_screen.dart
@@ -1,8 +1,8 @@
+import 'dart:convert';
 
-
-import 'package:clima_flutter/screens/location_screen.dart';
 import 'package:clima_flutter/services/location_service.dart';
 import 'package:clima_flutter/services/weather_api_service.dart';
+import 'package:clima_flutter/ui/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -21,16 +21,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await locationService.getCurrentLocation();
     print('${locationService.latitude}, ${locationService.longitude}');
 
-    //2. 현재좌표를 이용해서 날씨 API 서비스에서 날씨정보를 요청한다.
+    //2. 현재좌표를 이용해서 날씨API 서비스에서 날씨정보를 요청한다.
     WeatherApiService weatherApiService = WeatherApiService();
-    //2-1. 날씨정보를 Json 데이터로 확보
+    // 2-1. 날씨정보를 JsON 데이터로 확보
     var weatherDataJson = await weatherApiService.getWeatherInfoWithLocation(
-      latitude: locationService.latitude, longitude: locationService.longitude);
+        latitude: locationService.latitude, longitude: locationService.longitude);
 
     print(weatherDataJson);
 
-    //3.location 화면으로 이동
-    _navigateNextPage(context);
+    //3. location 화면으로 이동
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(weatherData: weatherDataJson);
+    }));
+
   }
 
   @override
@@ -38,13 +41,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
     super.initState();
     //1.날씨 정보 확보
     _getWeatherJsonData();
-  }
-
-  void _navigateNextPage(BuildContext context) {
-    Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const LocationScreen())
-    );
   }
 
   @override
@@ -55,7 +51,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
           color: Colors.purple,
           size: 100,
           lineWidth: 3.0,
-          itemCount: 5,
         ),
       ),
     );
